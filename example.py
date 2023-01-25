4510dd84b1c68cd0e4d0a8ffb286590042fc1dd3
evgeni_vasilev <e.vasyliev@inbox.ru>
2023-01-25 10:24:19 +0300

    creat readme project

---
+++ example.py
@@ -0,0 +1,7 @@
+import pandas as pd
+countries = pd.Series(
+    data = ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ'],
+    name = 'countries'
+)
+print(countries)
